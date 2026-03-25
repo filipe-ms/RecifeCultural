6b87dde021cc53f9a7e0c5533c0049f8119fb94b
Luís Santos <lfms@EU3146022016191.local>
2026-03-25 10:29:16 -0300

    Fix: SpringBoot configuration

---
+++ presentation-backend/src/main/java/recifecultural/RecifeCulturalApplication.java
@@ -0,0 +1,11 @@
+package recifecultural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecifeCulturalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RecifeCulturalApplication.class, args);
+    }
+}
